495c7f708ca370cbbd33d97edaaa6f109c04964c
radupadurariu <padurariuionutradu@gmail.com>
2024-10-30 14:31:45 +0200

    Add footer section

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,38 @@
+import { linkList } from "../../assets/data/data";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <>
+      <footer className="tj-footer-area">
+        <div className="container text-center">
+          <div className="logo-box">
+            <a href="/">
+              <img src="../images/common/logo.png" alt="logo" />
+            </a>
+          </div>
+          <div className="footer-menu">
+            <nav>
+              <ul>
+                {linkList.map((link) => (
+                  <li key={link.id}>
+                    <a href={link.link}>{link.text}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="copy-text">
+            <p>
+              &copy; 2024 All rights reserved by Radu Padurariu
+              {/* <a href="#" target="_blank">
+                GorkCoder
+              </a> */}
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
