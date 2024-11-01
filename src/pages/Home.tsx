34e4d6a72270fee4f6a9f46d6665b63aed97663a
radupadurariu <padurariuionutradu@gmail.com>
2024-11-01 13:23:17 +0200

    Add Project section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "../screen/Hero";
+import { Projects } from "../screen/Projects";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Projects />
     </div>
   );
 };
